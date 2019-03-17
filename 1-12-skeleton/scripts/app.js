1e24d759d5c1f022bb1659e279431b6d744ac7ab
Rob <rob_abcba@yahoo.com>
2019-03-17 11:50:53 -0700

    Inject dummy initial weather forecast data into JavaScript file app.js and immediately display it using the method app.updateForecastCard(injectedForecast);

--- 1-12-skeleton/scripts/app.js
+++ 1-12-skeleton/scripts/app.js
@@ -2,6 +2,35 @@
 (function() {
   'use strict';
 
+  // RL Start Add injected dummy forecast data to display the 1st time user opens this app
+  var injectedForecast = {
+    key: 'cupertino',
+    label: 'Cupertino, CA',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+  // RL End 
+
   var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
 
   var app = {
@@ -150,4 +179,11 @@
     });
   };
 
+  // RL Start
+  app.updateForecastCard(injectedForecast);
+  // RL End
+
 })();
+
+
+
